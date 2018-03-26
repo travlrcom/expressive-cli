a887ce1ecb8fd9bd2835a351018bd32d688005e9
Krisan Alfa Timur <krisan47@gmail.com>
2018-03-26 19:57:35 +0800

    [BUMP] 1.1.1

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const ghDownload = require('github-download')
 import * as request from 'request-promise-native'
 
 program
-  .version('1.1.0')
+  .version('1.1.1')
   .command('new [project]')
   .description('Create new project under given directory.')
   .action(async (project) => {
